77a47c81f6a81917c5b554ebc2148ccf77144832
Riya Agrahari <agrahaririya5892@gmail.com>
2026-05-28 21:02:25 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,3 @@
 import app from "./api/index.js";
-import loadEnv from "./config/loadEnv.js";
 
-loadEnv();
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`DocEase backend running on http://localhost:${port}`);
-});
+export default app;
